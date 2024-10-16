48f0ee9b11903caaedac1c2a0fb75510d6a2a7e8
yannickpichardo <yannickpichardo@gmail.com>
2024-10-16 19:26:39 +0200

    feat: updated key metrics

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 from constants import STOCKS, TRADING_DAYS_CRYPTO
 from forecasting import get_forecast
 from graphing import plot_candlestick_data, plot_forecast
-from key_metrics import display_key_metrics
 from yahoo import load_dataset
 
 # streamlit page configuration
@@ -16,18 +15,18 @@ selected_stock = st.selectbox("Select stock", STOCKS)
 # Hyperparameter controls for advanced tuning
 st.sidebar.header("Advanced Forecasting Settings")
 seasonality_mode = st.sidebar.selectbox(
-    "Seasonality Mode", ["additive", "multiplicative"]
+    "Seasonality Mode", ["multiplicative", "additive"]
 )
-changepoint_scale = st.sidebar.slider("Change Point Prior Scale", 0.01, 0.5, 0.05)
+changepoint_scale = st.sidebar.slider("Change Point Prior Scale", 0.01, 0.5, 0.20)
 yearly_seasonality = st.sidebar.checkbox("Yearly Seasonality", True)
 weekly_seasonality = st.sidebar.checkbox("Weekly Seasonality", True)
 
 
 # Load the dataset
-data_load_state = st.text("Loading data...")
+data_load_state = st.success("Loading data...")
 data = load_dataset(selected_stock)
 if data is not None:
-    data_load_state.text("Loading data...completed!")
+    data_load_state.success("Loading data...completed!")
 else:
     st.stop()
 
@@ -36,7 +35,15 @@ df_train = data[["Date", "Close"]].rename(columns={"Date": "ds", "Close": "y"})
 
 # Display key metrics
 st.subheader(f"Key Metrics for {selected_stock}")
-display_key_metrics(data)
+# display_key_metrics(data)
+col1, col2, col3 = st.columns(3)
+col1.metric(
+    "Current Price",
+    f"${data['Close'].iloc[-1]:,.2f}",
+    delta=f"{data['Close'].iloc[-1] - data['Close'].iloc[-2]}",
+)
+col2.metric("Highest Price", f"${data['Close'].max():,.2f}")
+col3.metric("Lowest Price", f"${data['Close'].min():,.2f}")
 
 
 # Function to plot historical candlestick data
